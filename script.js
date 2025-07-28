a68a9784c3489073ff6f8fa48f8998e8390b1a8f
mdahmed4875 <140884439+mdahmed4875@users.noreply.github.com>
2025-07-28 09:03:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
-}
+	let len=str.length;
+	let freq={};
+	for(let i=0;i<len;i++){
+		freq[str.charAt(i)]=(freq[str.charAt(i)]||0)+1;
+	}
+	for(let i=0;i<len;i++){
+		if(freq[str.charAt(i)]===1)return str.charAt(i);
+	}
+	return null;
+	
+} 
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
